2b6586f7f3137e2b8be1ab9cd4fbc5cf84f41426
Unknown <felixeisenmenger@gmx.net>
2019-03-05 23:04:11 +0100

    [UPDATE]
    step closer to the final result

--- EulerianCycleProb.py
+++ EulerianCycleProb.py
@@ -9,37 +9,48 @@ import copy
 import time
 from random import choice
 
+def graph(starting, node_edge):
+    b = copy.deepcopy(node_edge)
+    steps = []
+    x = starting
+    steps.append(starting)
+    while True:
+        if len(b[x]) >= 1:
+            x, y = b[x][0], x
+            b[y] = list([i for i in b[y] if i != x])
+            steps.append(x)
+        else:
+            x = None
+            break
+    return steps, b
+
+def ordering_edges(edges, node_edge):
+    edge = dict()
+    for i in edges:
+        g, _ = graph(i, node_edge)
+        edge[i] = len(g)
+    #edge = list([i[0] for i in sorted(edge.items(), key=lambda x: x[1], reverse=True)])
+    return list(reversed(list(edge.keys())))
+
 def eulerian_cycle_prob(node_edges_list):
     node_edge = dict()
     cycles = []
     keys = []
-    stop= '0'
     for index, elem in enumerate(node_edges_list):
         x = elem.replace(' ','').split('->')
         node_edge[x[0]] = list([i.strip() for i in x[1].split(',')])
+    for key, val in node_edge.items():
+        if len(val) > 1:
+            node_edge[key] = ordering_edges(val, node_edge)
     length = sum(list([len(i) for i in node_edge.values()]))
     keys = list([i for i in node_edge.keys()])
-    stop = node_edge[keys[0][0]][0] if node_edge[node_edge[keys[0][0]][0]][0] != keys[0] else keys[0]
-    xkeys = list([i for i in keys if i == stop])
-    for i in range(length):
-        for x in xkeys:
-            cycle = []
-            tmp = copy.deepcopy(node_edge)
-            while True:
-                cycle.append(x)
-                try:
-                    x, y = choice(tmp[x]), x
-                except (IndexError, TypeError):
-                    break
-                tmp[y] = list([i for i in tmp[y] if i != x])
-            if cycle[0] == cycle[-1] and len(cycle) == length + 1 and cycle[0] == stop:
-                cycles.append(cycle)
-                break
+    for x in keys:
+        result, _ = graph(x, node_edge)
+        if len(result) == length + 1:
+            cycles.append(result)
     return cycles
-                
-    
-    
-    
+
+
 if __name__ == '__main__':
     edges = [
             '0 -> 3',
@@ -53,5 +64,26 @@ if __name__ == '__main__':
             '8 -> 7',
             '9 -> 6',
             ]
+#    edges = [
+#            '1 -> 10',
+#            '10 -> 2,3,4',
+#            '2 -> 1',
+#            '3 -> 10',
+#            '4 -> 5',
+#            '5 -> 10',
+#            ]
+#    edges = [
+#            '0 -> 1,2,3,4',
+#            '1 -> 0,2,3,4',
+#            '2 -> 0,1,3,4',
+#            '3 -> 0,1,2,4',
+#            '4 -> 0,1,2,3',
+#            ]
+    node_edge = dict()
+    for index, elem in enumerate(edges):
+        x = elem.replace(' ','').split('->')
+        node_edge[x[0]] = list([i.strip() for i in x[1].split(',')])
+        
+#    print(ordering_edges(['1', '2', '3', '4'], node_edge))
     for i in eulerian_cycle_prob(edges):
         print(i)
